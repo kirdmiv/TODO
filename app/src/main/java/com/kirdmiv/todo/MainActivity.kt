c8dbe39a96154207165f74d75ea472cd148563c1
Kirill Ivanov <kirdmiv@users.noreply.github.com>
2020-06-20 19:25:53 +0300

    Add swipes

--- app/src/main/java/com/kirdmiv/todo/MainActivity.kt
+++ app/src/main/java/com/kirdmiv/todo/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.kirdmiv.todo
 
 import android.content.Intent
+import android.graphics.Canvas
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.widget.AdapterView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator
 import kotlinx.android.synthetic.main.content_main.*
 
 
@@ -42,6 +45,73 @@ class MainActivity : AppCompatActivity() {
         linearLayoutManager = LinearLayoutManager(this)
         notesLv.layoutManager = linearLayoutManager
         notesLv.adapter = noteAdapter
+
+        val mIth = ItemTouchHelper(
+            object : ItemTouchHelper.SimpleCallback(
+                0,
+                ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+            ) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: ViewHolder, target: ViewHolder
+                ): Boolean {
+                    return false
+                }
+
+                override fun onSwiped(
+                    viewHolder: ViewHolder,
+                    direction: Int
+                ) {
+                    val pos: Int = viewHolder.adapterPosition
+
+                    when (direction){
+                        ItemTouchHelper.RIGHT -> {
+                            notes[pos].completed = true
+                            Log.d("Swiped", notes[pos].completed.toString())
+                            noteAdapter.notifyItemChanged(pos)
+                        }
+                        ItemTouchHelper.LEFT -> {
+                            val deletedNote = notes.removeAt(pos)
+                            noteAdapter.notifyItemRemoved(pos)
+                            Snackbar.make(notesLv, deletedNote.msg, Snackbar.LENGTH_LONG)
+                                .setAction("UNDO") {
+                                    notes.add(pos, deletedNote)
+                                    Log.d("Swiped", notes[pos].msg)
+                                    noteAdapter.notifyItemInserted(pos)
+                                }.show()
+                        }
+                    }
+                }
+
+                override fun onChildDraw(
+                    c: Canvas,
+                    recyclerView: RecyclerView,
+                    viewHolder: ViewHolder,
+                    dX: Float,
+                    dY: Float,
+                    actionState: Int,
+                    isCurrentlyActive: Boolean
+                ) {
+                    RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                        .addSwipeLeftBackgroundColor(ContextCompat.getColor(applicationContext, R.color.bad))
+                        .addSwipeLeftActionIcon(R.drawable.ic_baseline_delete_24)
+                        .addSwipeRightBackgroundColor(ContextCompat.getColor(applicationContext, R.color.good))
+                        .addSwipeRightActionIcon(R.drawable.ic_baseline_check_24)
+                        .create()
+                        .decorate();
+
+                    super.onChildDraw(
+                        c,
+                        recyclerView,
+                        viewHolder,
+                        dX,
+                        dY,
+                        actionState,
+                        isCurrentlyActive
+                    )
+                }
+            })
+            mIth.attachToRecyclerView(notesLv)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
